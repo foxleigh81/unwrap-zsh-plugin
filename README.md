2aa2b296cf693c4ef16f83d1533d9458116eb3c9
Alex Foxleigh <alex@alexfoxleigh.com>
2022-02-25 20:51:01 +0000

    Initial commit and release

---
+++ README.md
@@ -0,0 +1,45 @@
+# Unwrap: A plugin for ZSH
+
+This simple plugin allows you to remove a directory without removing its contents.
+
+## Usage
+
+In your `zsh` command line, navigate into the directory you want to unwrap,
+then type `unwrap`. You will be given a prompt to confirm the action and if you
+confirm, unwrap will remove the current directory and move all of its contents
+into the parent folder.
+
+## Installation
+
+If you are using oh-my-zsh:
+
+Copy the branch folder to `.oh-my-zsh/custom/plugins`, for example:
+
+```bash
+cd ~/.oh-my-zsh/custom/plugins
+git clone git@github.com:foxliegh81/unwrap-zsh-plugin.git unwrap
+```
+
+Enable the plugin in your `.zshrc` file. Edit `~/.zshrc` to activate the plugin, for example: `plugins=(unwrap)`
+And restart the terminal to apply (or just type `source ~/.zshrc`).
+
+
+## Protected Directories
+
+Unwrap will not remove any directories that are in the root of your system 
+(or any directory with the same name as one of those folders). Nor will it 
+remove the users home directory.
+
+You can add custom-protected directories by creating an `.unwrap` file in your
+home directory. The file should contain a list of directories to protect.
+
+An example of a `.unwrap` file can be found in the plugin install directory.
+
+If your folder is not covered by the above, use `unwrap` with caution.
+
+### A warning about folder names
+
+`Unwrap` protection is case-sensitive and will not protect folders that have a space, dash
+or other special character in their name. It will, however, work fine with
+folders that have an underscore in their name.
+
---
+++ unwrap.plugin.zsh
@@ -0,0 +1,50 @@
+#!/bin/zsh
+# Copies the contents of the current directory into the parent 
+# directory and then deletes the current directory
+unwrap() {
+  local this_dir=`basename $PWD`;
+  local sys_dirs=`ls /`;
+  local in_sys_dir=`echo ${sys_dirs[@]} | grep -o $this_dir | wc -w`;
+  local user_protected_dirs=`cat ~/.unwrap`
+  local in_user_protected_dirs=`echo ${user_protected_dirs[@]} | grep -o $this_dir | wc -w`;
+      
+  # Check for dangerous directories
+  if [[ $this_dir == '/' ]]; then
+    echo "\033[38;5;124mRoot directory detected. Aborting.";
+    return 0;
+  elif [[ in_sys_dir -gt 0 ]]; then
+    echo "\033[38;5;124mSystem directory detected. Aborting.";
+    return 0;
+  elif [[ in_user_protected_dirs -gt 0 ]]; then
+    echo "\033[38;5;124mUser protected directory detected. Aborting.";
+    return 0;
+  elif [[ $this_dir == `basename $HOME` ]]; then
+    echo "\033[38;5;124mHome directory detected. Aborting.";
+    return 0;
+  else
+    # The usual suspect have been eliminated. Proceed with a warning.
+    echo "\n\033[38;5;124mWarning:\n"
+    echo "\033[38;5;024mThis will move all files into the parent directory"
+    echo "and then delete this folder.\n"
+    echo "\033[38;5;124mExisting files will be overwritten.\n"
+    
+    echo -n "\033[38;5;024mDo you wish to proceed? (y/N)\n\033[0m";
+    
+    read response;
+    
+    # Check to see if the directory is empty
+    if [[ $response == "y" ]]; then
+      if [ -z "$(ls -A)" ]; then
+        # Empty directory. Move into parent directory and delete
+        echo "\n⚙️ \033[38;5;144m No files to move. Proceeding.\033[0m";
+      else
+        echo "\n⚙️ \033[38;5;144m Moving files"
+        cp -r * ../
+      fi
+      echo "\n✅ \033[38;5;118mFiles moved, removing directory"
+      cd ..
+      rm -rf $this_dir
+    fi
+    return 1
+  fi
+}
